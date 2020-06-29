6971ee14b47d9504b790b86552f0a3ad4436f0fe
Cleberson <Silva>
2020-06-28 22:09:01 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+
+  public readonly urlImage = 'https://t4.ftcdn.net/jpg/00/62/33/45/240_F_62334577_pPZSWcWMOUtv8HsB6sEwPIZ7yLi9NXS3.jpg';
+  public readonly altImage = 'Leão';
 }
