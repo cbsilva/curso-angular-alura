875e1781aea7c530cf893bd85d4237326044bec5
Cleberson <Silva>
2020-07-02 01:37:45 -0300

    segmentando as rotas 'melhorias'

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,13 +12,17 @@ export class PhotoListComponent implements OnInit {
   photos: any[] = [ ];
 
   constructor(
-    private photoService: PhotoService
+    private photoService: PhotoService,
+    private activatedRouter: ActivatedRoute
   ){ }
 
   ngOnInit(): void {
-    this.photoService.listFromUser('flavio')
+
+    // recupera a informacao da rota corrente
+    const username = this.activatedRouter.snapshot.params.username;
+
+    this.photoService.listFromUser(username)
     .subscribe((photos) => {
-      console.log(photos[0].postDate);
       this.photos = (photos);
   });
   }
