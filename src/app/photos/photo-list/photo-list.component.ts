273ac04cfe1326c465231b00c32106a54e7d1a50
Cleberson <Silva>
2020-07-04 21:02:34 -0300

    ajustando filto e utilizando output property

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { debounceTime } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -11,11 +8,11 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter = '';
-  debounce: Subject<string> = new Subject<string>();
+
   hasMore = true;
   currentPage = 1;
   userName = '';
@@ -25,16 +22,11 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     private photoService: PhotoService
   ){ }
 
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
-  }
+
 
   ngOnInit(): void {
     this.userName = this.activatedRouter.snapshot.params.userName;
     this.photos = this.activatedRouter.snapshot.data.photos;
-    this.debounce
-        .pipe(debounceTime(300))
-        .subscribe(filter => this.filter = filter);
   }
 
 
--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,13 +6,15 @@ import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { CardModule } from '../../shared/components/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     PhotoListComponent,
     PhotosComponent,
     LoadButtonComponent,
-    FilterByDescription
+    FilterByDescription,
+    SearchComponent
   ],
   imports: [
     CommonModule,
