98227416112056093a8c6c60c22fdff38d40a950
Cleberson <Silva>
2020-07-18 23:26:18 -0300

    enviando credenciais para api
    consumindo api de login

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   // selector: 'app-signin',
@@ -10,13 +11,37 @@ export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  public isMessage = false;
+  public message = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    const username = this.loginForm.get('username').value;
+    const password = this.loginForm.get('password').value;
+    this.authService
+        .authenticate(username, password)
+        .subscribe(
+          () => {
+            console.log('Autenticado');
+            this.isMessage = false;
+          },
+          err  => {
+            console.log(err);
+            this.message = 'Invalid user name or password';
+            this.isMessage = true;
+            this.loginForm.reset();
+          }
+        );
   }
 
 }
